902c0b9a7c6bd11564ece9a8c28d7d211a62e661
Laura Stroud <stroud@opsys.cs.umsl.edu>
2022-02-04 10:22:08 -0600

    committing

--- stroud.1.1/main.c
+++ stroud.1.1/main.c
@@ -9,7 +9,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/wait.h>
-
+#include <string.h>
 int main(int argc, char *argv[]){
         pid_t childpid = 0;
         int i;
@@ -17,11 +17,15 @@ int main(int argc, char *argv[]){
 	int sleeptime = 3;
 	int niters = 1;
 	int nchars = 80;
-	
+        FILE *file;
+        char *filename;
+	int j;
+	char ch;
+
 	//command line arguments
 	int opt;
 
-	while((opt = getopt(argc, argv, ":hp:c:s:i:")) != -1){
+	while((opt = getopt(argc, argv, ":hp:c:s:i:f:")) != -1){
 		switch(opt){
 printf("%d nprocs\n", nprocs);
                                 printf("%d sleeptime\n", sleeptime);
@@ -36,66 +40,91 @@ printf("%d nprocs\n", nprocs);
 				break;
 
 			case 'p'://number of procceses
-				printf("in switch p\n");
 				nprocs = atoi(optarg);
-
-				printf("%d nprocs\n", nprocs);
-				printf("%d sleeptime\n", sleeptime);
-				printf("%d nchars\n", nchars);
 				break;
 			case 'c':// number of characters to read
 				nchars = atoi(optarg);
-
-				printf("%d nprocs\n", nprocs);
-                                printf("%d sleeptime\n", sleeptime);
-                                printf("%d nchars\n", nchars);
 				break;
 			case 's'://sleeptime
 				sleeptime = atoi(optarg);
-
-				printf("%d nprocs\n", nprocs);
-                                printf("%d sleeptime\n", sleeptime);
-                                printf("%d nchars\n", nchars);
-				printf("%d niters\n", niters);
 				break;
 			case 'i':
 				niters = atoi(optarg);
 				break;
+			case 'f'://file name
+				filename = optarg;
+				break;
 			default:
-				printf("Usage: ./chain [-h] [-p nprocs] [-c nchars] [-s sleeptime] [-i niters] < textfile");
+				printf("Usage: ./chain [-h] [-p nprocs] [-c nchars] [-s sleeptime] [-i niters] < textfile\n");
 				perror("Error in switch: ");
 				break;
 			
 		}
 	}
-        
-	printf("out of while loop");
+
+
 
         for(i = 1; i < nprocs; i++){
-		printf("in 1st for loop\n");
                 if((childpid = fork()) <= 0){
                         break;
                 }
         }
-	
-	int j;
+
 	for(j = 0; j < niters; j++){
-		printf("in second for\n");
 		sleep(sleeptime);
 
 		wait(NULL);
-        //	fprintf(stderr, "i:%d process ID:%ld parent ID:%ld Child ID:%ld\n",
-          //              i, (long)getpid(), (long)getppid(), (long)childpid);
+        	//fprintf(stderr, "i:%d process ID:%ld parent ID:%ld Child ID:%ld\n", i, (long)getpid(), (long)getppid(), (long)childpid);
 
-		fprintf(stderr, "i:%d process ", i);
-		fprintf(stderr, "process ID:%ld ", (long)getpid());
-		fprintf(stderr, "parent ID:%ld \n ", (long)getppid());
+//		fprintf(stderr, "i:%d process ", i);
+//		fprintf(stderr, "process ID:%ld ", (long)getpid());
+//		fprintf(stderr, "parent ID:%ld \n ", (long)getppid());
 		//fprintf(stderr, "child ID:%ld \n", (long)childpid);
-		
-		
-		//loop that reads each character putting it into an array
+	} //END OF FOR LOOP
+
 	
-	}
+	//open file
+        file = fopen(filename, "r");
+        if(file == NULL){
+                perror("Error opening file: ");
+        }else{
+                printf("opened file");
+        }
+	
+	//initalize array
+	char *mybuf = malloc(sizeof *mybuf *  nchars);
+	int n = 0;
+	char string[200];
+	char *delim = " \n";
+	char *word;
+	char line[200];
+	int length;
+	int y;
+		while(fgets(string, sizeof string, file)){ //loop through file
+			if(strstr(string, line)){
+				//do nothing
+			}
+			for(word = strtok(string, delim);word;  word = strtok(NULL, delim)){
+				length = strlen(word);
+				for(y=0; y < length; y++){
+					//printf("%c \n ", word[y]);
+					mybuf[n] = word[y];
+					//printf("%c \n", mybuf[n]);
+					n++;
+				}
+			}
+		}
+		mybuf[n] = '\0';
+
+		fclose(file);
+
+		int x;
+		//print array
+		for(x = 0; x < n; x++){
+			printf("%c \n", mybuf[x]);
+		}
+
+		free(mybuf);//free memory
 
         return 0;
 }
